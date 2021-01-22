6bf50a9c1f1bf7d8f159e92fdd0d4b1aca1ab59c
miachell <mathilde.iachella@gmail.com>
2021-01-22 16:09:02 +0100

    #2 search form added

--- src/app/features/menu/home/components/home.component.ts
+++ src/app/features/menu/home/components/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -11,17 +11,20 @@ export class HomeComponent implements OnInit, OnDestroy {
 
   searchForm : FormGroup;
 
-  constructor(private router: Router) {
+  constructor(private formBuilder: FormBuilder,private router: Router) {
     
   }
 
   ngOnInit(): void { 
-    
+    this.searchForm = this.formBuilder.group({
+      township: ['', Validators.required]
+    })
   }
 
   ngOnDestroy(): void {}
 
   btnClick () {
     this.router.navigateByUrl('/indicators');
+    console.log(this.searchForm.get('township').value);
   }
 }
--- src/app/features/menu/indicator/indicator.module.ts
+++ src/app/features/menu/indicator/indicator.module.ts
@@ -3,13 +3,15 @@ import { NgModule } from "@angular/core";
 import { IndicatorComponent } from "./components/indicator.component";
 import { SharedModule } from '../../../shared/shared.module';
 import { IndicatorRoutingModule } from "./indicator-routing.module";
+import { HomeModule } from "../home/home.module";
 
 @NgModule({
     declarations: [IndicatorComponent],
     imports: [
         CommonModule,
         IndicatorRoutingModule, 
-        SharedModule
+        SharedModule, 
+        HomeModule
     ]
   })
   export class IndicatorModule { }
