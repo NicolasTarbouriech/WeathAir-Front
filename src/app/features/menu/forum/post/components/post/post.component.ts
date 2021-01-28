d631cb548cf5323aba46d2d67d9a7820175c6155
Nicolas Tarbouriech <65289313+NicolasTarbouriech@users.noreply.github.com>
2021-01-28 18:33:48 +0100

    forum is not finish yet

--- src/app/features/menu/forum/post/components/post/post.component.ts
+++ src/app/features/menu/forum/post/components/post/post.component.ts
@@ -14,33 +14,56 @@ export class PostComponent implements OnInit {
   messageName :string;
   printPostForm = false;
   postList: Post[]= [];
+  messageList : Message[] = [];
   postName : string;
   postTitle : string;
-  idTopic : number;
-
+  topic_id : number;
+  post_id : number;
+  getPostById : Post [];
   constructor(private postService: PostService, 
-    private router:Router, private route: ActivatedRoute) { }
+    private router:Router, private route: ActivatedRoute) {
+      
+    }
 
   ngOnInit(): void {
     this.printPostForm;
-    this.idTopic = parseInt(this.route.snapshot.paramMap.get("id"));
-    console.log(this.idTopic);
-
-    this.postService.getPostById(this.idTopic).subscribe( result => {
-      this.postList = result;
-      console.log(result);
-    }, err => {
-      console.log(err)
-    })
+    this.topic_id = parseInt(this.route.snapshot.paramMap.get("id"));
+    this.post_id = parseInt(this.route.snapshot.paramMap.get("id"));
+    console.log(this.topic_id);
+    console.log(this.post_id);
+      this.showPostById();
+      this.showMessageById();
   }
 
   printPostToTrue(){
     this.printPostForm = true;
   }
 
+  showPostById() {
+  return this.postService.getAllPostsByTopic(this.topic_id)
+      .subscribe(result => {
+            this.postList = result;
+            console.log(result);
+        },
+        error => {
+          console.log(error);
+        });
+  }
+
+  showMessageById() {
+    return this.postService.getAllMessages(this.topic_id, this.post_id)
+        .subscribe(result => {
+              this.messageList = result;
+              console.log(result);
+          },
+          error => {
+            console.log(error);
+          });
+    }
+
   createPost(){
     var post = new Post({"text": this.postName, "title": this.postTitle});
-    this.postService.postPost(post).subscribe(
+    this.postService.postPost(this.topic_id, post).subscribe(
       // data => this.topicList.push(data),
       err => console.log(err)
     );
@@ -48,7 +71,7 @@ export class PostComponent implements OnInit {
 
   createMessage(){
     var message = new Message({"text": this.messageName});
-    this.postService.postMessage(message).subscribe(
+    this.postService.postMessage(this.topic_id, this.post_id, message).subscribe(
       // data => this.topicList.push(data),
       err => console.log(err)
     );
