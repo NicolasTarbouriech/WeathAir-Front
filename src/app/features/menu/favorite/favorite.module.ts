10ab338fcdadd7f43a58f1c4ea6713cb7040c104
miachell <mathilde.iachella@gmail.com>
2021-01-29 15:03:50 +0100

    #9 Title OK, import of ngx-echarts for the presentation of favorite data

--- src/app/features/menu/favorite/favorite.module.ts
+++ src/app/features/menu/favorite/favorite.module.ts
@@ -3,16 +3,18 @@ import { NgModule } from "@angular/core";
 import { FavoriteComponent } from "./components/favorite.component";
 import { SharedModule } from '../../../shared/shared.module';
 import { FavoriteRoutingModule } from "./favorite-routing.module";
-import { MatAutocomplete, MatAutocompleteModule } from "@angular/material/autocomplete";
 import { AddIndicatorComponent } from "./modals/add-indicator/add-indicator.component";
 import { DetailsComponent } from './components/details/details/details.component';
+import { NgxEchartsModule } from "ngx-echarts";
+import * as echarts from 'echarts';
 
 @NgModule({
     declarations: [FavoriteComponent, AddIndicatorComponent, DetailsComponent],
     imports: [
         CommonModule,
         FavoriteRoutingModule, 
-        SharedModule
+        SharedModule, 
+        NgxEchartsModule.forRoot({echarts})
     ]
   })
   export class FavoriteModule { }
